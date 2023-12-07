b888b92b582dec5eaff1abf061a73145b99b73b8
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-12-07 12:50:13 +0600

    index route added

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -15,15 +16,14 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-const getAController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get('/', getAController);
+app.get('/', test);
 
 //global error handler
 
---
+++ src/app/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { StudentRoutes } from '../modules/student/student.route';
+import { UserRoutes } from '../modules/user/user.route';
+
+const router = Router();
+
+router.use('/users', UserRoutes);
+router.use('/students', StudentRoutes);
+
+export default router;
